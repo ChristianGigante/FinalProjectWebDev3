2c7987a58c6376fa07b5dbd0bcf69e4f7769df46
gigantech_sd2023@CEACCS.NET <gigantech_sd2023@CEACCS.NET>
2019-10-29 17:07:14 +0800

    ..,

--- vuechan/src/views/ListOfDev.vue
+++ vuechan/src/views/ListOfDev.vue
@@ -8,34 +8,35 @@
 
 <script>
 export default {
+  props:["groups"],
   data() {
     return {
       groups: [
         {
           images: "../assets/img1.png",
           name: "Christian Gigante",
-          position: "a",
+          position: "Project Manager",
           motto:
             "First, have a definite, clear practical ideal; a goal, an objective. Second, have the necessary means to achieve your ends; wisdom, money, materials, and methods. Third, adjust all your means to that end. ~ Aristotle "
         },
         {
           images: "../assets/img1.png",
           name: "Chilla Jean Cabungcag",
-          position: "a",
+          position: "Backend Developer",
           motto:
             "Let our advance worrying become advance thinking and planning. ~ Winston Churchill"
         },
         {
           images: "../assets/img1.png",
           name: "Marion Jay Balugo",
-          position: "a",
+          position: "Frontend Developer",
           motto:
             "Plans are worthless. Planning is essential. ~ Dwight D. Eisenhower "
         },
         {
           images: "../assets/img1.png",
           name: "Hannah Mae Peliño",
-          position: "a",
+          position: "Frontend Developer",
           motto:
             "A good plan can help with risk analyses but it will never guarantee the smooth running of the project.  ~ Bentley and Borman"
         }
