200126aa380a088ffd2103154d46f0364dbb7aee
gigantech_sd2023@CEACCS.NET <gigantech_sd2023@CEACCS.NET>
2019-11-05 16:04:58 +0800

    ended

--- vuechan/src/views/ListOfDev.vue
+++ vuechan/src/views/ListOfDev.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <b-card v-for="(element, i) in groups" :key="i" img-src="element.images" img-alt="element.name" img-left class="mb-3">
-      <img v-bind:src="element.images" alt="" srcset="">
-      <p>{{element.name}}</p>
+    <b-card v-for="(element, i) in groups" :key="i" :img-src="element.pic" :img-alt="element.name" img-left class="mb-3">
+      <h3>{{element.name}}</h3>
+      <h5>{{element.position}}</h5>
       <b-card-text>{{ element.motto }}</b-card-text>
     </b-card>
   </div>
