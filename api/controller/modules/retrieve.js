075f9df7bef291ca02f54ca4ac47c6d877908803
Christian Gigante <54196546+ChristianGigante@users.noreply.github.com>
2019-11-06 21:32:14 +0800

    added comments

--- api/controller/modules/retrieve.js
+++ api/controller/modules/retrieve.js
@@ -1,5 +1,7 @@
 let retrieveOne = (model,req,res) => {
-
+// model - from server to database
+// req - from client to server
+// res - from server to client
 }
 let retrieveAll = (model,req,res) => {
 
